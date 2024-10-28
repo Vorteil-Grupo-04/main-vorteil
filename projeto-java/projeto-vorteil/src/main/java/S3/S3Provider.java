e35fd39d2382b30f1eb8f5d8d029ae08fb44d543
FelipeSouza <antonio.souza@sptech.school>
2024-10-28 01:02:17 -0300

    ec2 conectada e jar ok

--- projeto-java/projeto-vorteil/src/main/java/S3/S3Provider.java
+++ projeto-java/projeto-vorteil/src/main/java/S3/S3Provider.java
@@ -6,7 +6,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 
 public class S3Provider {
 
-    private final AwsSessionCredentials credentials;
+
 
     public S3Provider() {
         System.out.println("Access Key: " + System.getenv("AWS_ACCESS_KEY_ID"));
@@ -14,17 +14,12 @@ public class S3Provider {
         System.out.println("Session Token: " + System.getenv("AWS_SESSION_TOKEN"));
 
         // Criando credenciais
-        this.credentials = AwsSessionCredentials.create(
-                System.getenv("AWS_ACCESS_KEY_ID"),
-                System.getenv("AWS_SECRET_ACCESS_KEY"),
-                System.getenv("AWS_SESSION_TOKEN")
-        );
+
     }
 
     public S3Client getS3Client() {
         return S3Client.builder()
                 .region(Region.US_EAST_1)
-                .credentialsProvider(() -> credentials)
                 .build();
     }
 }
